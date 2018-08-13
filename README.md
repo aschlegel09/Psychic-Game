182b1d0a9bfbfc5dd2299ef91aafc02fb7e6c688
aschlegel <aschlegel09@gmail.com>
2018-08-13 07:25:27 -0400

    first js commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Word Guess Game
+# Based on the theme, choose a letter to guess 
+# who the movie protagonist is.
+# 
+#
+<!-- 
+1. Pick a random word.
+2. Take the player’s guess.
+3. Quit the game if the player wants to.
+4. Check that the player’s guess is a valid letter.
+5. Keep track of letters the player has guessed.
+6. Show the player their progress.
+7. Finish when the player has guessed the word. 
+-->
---
+++ assets/javascript/game.js
@@ -0,0 +1,133 @@
+// Pick a random word
+// 
+// While the word has not been guessed {
+// Show the player their current progress
+// Get a guess from the player
+// If the player wants to quit the game {
+// Quit the game
+// }
+// Else If the guess is not a single letter {
+// Tell the player to pick a single letter
+// }
+// Else {
+// If the guess is in the word {
+// Update the player's progress with the guess
+// }
+// }
+// }
+// Congratulate the player on guessing the word
+
+var words = [
+    "ferris",
+    "sabrina",
+    "batman",
+    "ferris",
+    "ferris",
+    "ferris",
+    "ferris",
+    "ferris",
+    "ferris",
+]
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+
+var userGuess = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+
+var moviePros = {
+  //keys are strings; need quotes with spaces
+  "real name": "Gandalf",
+  "age (est)": 11000,
+  race: "Maia",
+  haveRetirementPlan: true,
+  aliases: [
+    "Greyhame",
+    "Stormcrow",
+    "Mithrandir",
+    "Gandalf the Grey",
+    "Gandalf the White"
+  ]
+};
+
+// Creating variables to hold the number of wins, losses, and ties. They start at 0.
+var wins = 0;
+var guessAmt = 0;
+var lettersUsed = 0;
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+  // Determines which key was pressed.
+  var userGuess = event.key;
+
+    if (guess = userGuess) {
+            var guess = prompt("What's your guess?");
+    console.log(guess);
+
+}}
+
+
+  // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+//   if (userGuess === "r" || userGuess === "p" || userGuess === "s") {
+//     if (userGuess === "r" && computerGuess === "s") {
+//       wins++;
+//     } else if (userGuess === "r" && computerGuess === "p") {
+//       losses++;
+//     } else if (userGuess === "s" && computerGuess === "r") {
+//       losses++;
+//     } else if (userGuess === "s" && computerGuess === "p") {
+//       wins++;
+//     } else if (userGuess === "p" && computerGuess === "r") {
+//       wins++;
+//     } else if (userGuess === "p" && computerGuess === "s") {
+//       losses++;
+//     } else if (userGuess === computerGuess) {
+//       ties++;
+//     }
+
+    // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+    // var html =
+    //   "<p>You chose: " +
+    //   userGuess +
+    //   "</p>" +
+    //   "<p>wins: " +
+    //   wins +
+    //   "</p>" +
+    //   "<p>losses: " +
+    //   losses +
+    //   "</p>" +
+    //   "<p>ties: " +
+    //   ties +
+    //   "</p>";
+
+    // Set the inner HTML contents of the #game div to our html string
+//     document.querySelector("#game").innerHTML = html;
+//   }
+

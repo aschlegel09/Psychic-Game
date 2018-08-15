018743583ba260a85b848d7abb6c7be8fc050b92
Andy <28871636+aschlegel09@users.noreply.github.com>
2018-08-15 16:57:46 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,35 @@
+window.onload = function () {
+var alphabet = [  
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];	
 // Pick a random word
-// 
-// While the word has not been guessed {
-// Show the player their current progress
-// Get a guess from the player
-// If the player wants to quit the game {
-// Quit the game
-// }
-// Else If the guess is not a single letter {
-// Tell the player to pick a single letter
-// }
-// Else {
-// If the guess is in the word {
-// Update the player's progress with the guess
-// }
-// }
-// }
-// Congratulate the player on guessing the word
-
-
 //an array of words to start the game, lowercase
-var words = [
+var wordList = [
     "jack sparrow",
     "han solo",
     "clark kent",
@@ -64,98 +74,131 @@ var words = [
     "derek zoolander",
     "ron burgundy",
     "borat",
-]
-
-//picks a random word from the chosen array, names new var
-var word = words[Math.floor(Math.random() * words.length)];
-
-//for loop variable i starts at 0 and goes up to but does not include word.length, and eacg time we add a new element to answer
-//when the loop is finished the answer will be the same length as the word
-var answer = [];
-    for (var i = 0; i < word.length; i++) {
-        answer[i] = "_";
-    }
-
-//keeps track if how many letters are left to guess, reduced by one when a player guesses correct
-var remaining = word.length;
-
-//while loop continues loop as long as remaining variable is greater than 0
-//needs to update for every correct guess, loop ends when it equals 0
-while (remaining > 0) {
- // Game code goes here
- // Show the player their progress
- // Take input from the player
- // Update answer and remaining for every correct guess
-    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-        if (guess === null) {
-            break;//immediately stops loop, even while condition is true
-        } else if (guess.length !== 1) {
-            alert("Please enter a single letter.");
-        } else {
- // Update the game state with the guess
-}
-}
-
-var userGuess = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
 ];
+var guess; //user guess defined
+var correctLetter = []; //stores correctly guessed letters
+var wrongLetter = []; //stores incorrectly guessed letters
+var counter; //counts correct letters
+var lives; //counts user lives
+var space; //for number of spaces in word '-'
+// pick a random word from the chosen array, name new var
+var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+// Get elements
+var showLives = document.getElementById("lives");
 
+	// create alphabet ul
+var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
 
-var moviePros = {
-  //keys are strings; need quotes with spaces
-  "real name": "Gandalf",
-  "age (est)": 11000,
-  race: "Maia",
-  haveRetirementPlan: true,
-  aliases: [
-    "Greyhame",
-    "Stormcrow",
-    "Mithrandir",
-    "Gandalf the Grey",
-    "Gandalf the White"
-  ]
-};
-
-// Creating variables to hold the number of wins, losses, and ties. They start at 0.
-var wins = 0;
-var guessAmt = 0;
-var lettersUsed = 0;
-
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+	
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
   // Determines which key was pressed.
-  var userGuess = event.key;
+  var guess = event.key;
+  
+}
+	
+	
 
-    if (guess = userGuess) {
-            var guess = prompt("What's your guess?");
-    console.log(guess);
+//chosen word is replaced with
+//for loop variable i starts at 0 and goes up to but does not include word.length, and each time we add a new element to answer
+//when the loop is finished the answer will be the same length as the word
+function start() {
+	for (var i = 0; i < randomWord.length; i++) {
+        correctLetter[i] = "_";
+}
+//var answer = [];
+//    for (var i = 0; i < randomWord.length; i++) {
+//        answer[i] = "_";
+//    }
 
-}}
+document.getElementById("answer").innerHTML = correctLetter.join(" ");
+  console.log(randomWord);
+}
+//
+//function checkLetter() {
+//  document.onkeyup = function(event) {
+//    guess = event.key.toLowerCase();
+//    var found = false; //lets use bool to check if a letter was found
+//    for (i = 0; i < randomWord.length; i++) {
+//      if (guess === randomWord[i]) {
+//        correctLetter[i] = guess;
+//        document.getElementById("answer").innerHTML = correctLetter.join("");
+//        found = true;
+//      }
+//    }
+//    //now all letters have been checked, was any found
+//    if (found) return; //if yes return
+//    
+//    //wrong, lets also see if 
+//    //it's not already logged, if not add it
+//    //you could also then use  wrongLetters.length
+//    //for working out if gueses area all used up.
+//    if (wrongLetters.indexOf(guess) < 0) {
+//      wrongLetters.push(guess);
+//      document.getElementById("guess").innerHTML = wrongLetters.join("");
+//    }
+//  }
+//}
+//
+//start();
+//checkLetter();
+//
+//	
+////keeps track of how many letters are left to guess, need to reduce by one when a player guesses correct
+//var remaining = randomWord.length;
+//
+//// While the word has not been guessed {
+////while loop continues loop as long as remaining variable is greater than 0
+////needs to update for every correct guess, loop ends when it equals 0
+//while (remaining > 0) {
+// // Game code goes here
+//// Show the player their current progress
+// // Take input from the player
+// // Update answer and remaining for every correct guess
+//    var guess = function();
+//// Get a guess from the player
+//        if (guess === null) {// If the player wants to quit the game {
+//            break;//immediately stops loop, even while condition is true
+//        } else if (randomWord.length !== 1) {// Else If the guess is not a single letter {
+//            alert("Please enter a single letter.");
+//// Tell the player to pick a single letter
+//        } else {
+//// If the guess is in the word {
+//			if (guess === randomWord[i]){
+//				
+//			}
+//}// Update the player's progress with the guess
+//}
+
+
+
+
+
+
+// Creating variables to hold the number of wins, losses, and ties. They start at 0.
+//var wins = 0;
+//var guessAmt = 0;
+//var lettersUsed = 0;
+//
+
+//
+//    if (guess = userGuess) {
+//            var guess = prompt("What's your guess?");
+//    console.log(guess);
+//
+//}};
 
 
   // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
@@ -195,3 +238,4 @@ document.onkeyup = function(event) {
 //     document.querySelector("#game").innerHTML = html;
 //   }
 
+}
